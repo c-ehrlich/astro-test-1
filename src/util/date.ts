fcfead5580ce328d774bcc1f6f1624bdcf798574
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-23 21:13:45 +0200

    feat: Add type guard to getFullDate

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,4 +1,8 @@
 export function getFullDate(date: string): string {
+  if (!date || typeof date !== 'string') {
+    return 'wrong datatype passed to getFullDate';
+  }
+
   if (!date.match(/^\d{4}\/\d{2}\/\d{2}$/))
     return 'getFullDate got invalid input';
 
