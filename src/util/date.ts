6c31ad089819e1c2e0bba35636b0627a5cf0237e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-23 20:06:25 +0200

    feat: getFullDate utility function

---
+++ src/util/date.ts
@@ -0,0 +1,44 @@
+export function getFullDate(date: string): string {
+  if (!date.match(/^\d{4}\/\d{2}\/\d{2}$/))
+    return 'getFullDate got invalid input';
+
+  const [year, month, day] = date.split('/');
+  const d = new Date(Number(year), Number(month), Number(day));
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return `${months[d.getMonth() - 1]} ${day}, ${year}`;
+}
+
+export function getMonthYearDate(date: string): string {
+  if (!date.match(/^\d{4}\/\d{2}\/\d{2}$/))
+    return 'getFullDate got invalid input';
+
+  const [year, month, _] = date.split('/');
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return `${months[Number(month) - 1]} ${year}`;
+}
